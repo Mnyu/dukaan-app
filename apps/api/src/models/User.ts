28186cc10a0d5a46675252fd7702d3c1a47a0de5
Abhimanyu <mail2abhimanyu.gupta@gmail.com>
2023-08-04 14:58:36 +0530

    min length for password = 1

--- apps/api/src/models/User.ts
+++ apps/api/src/models/User.ts
@@ -37,7 +37,7 @@ const UserSchema = new mongoose.Schema<UserInterface>({
   password: {
     type: String,
     required: [true, 'Please provide password'],
-    minLength: 6,
+    minLength: 1,
   },
   role: {
     type: String,
--- packages/common/src/zod-types/user.ts
+++ packages/common/src/zod-types/user.ts
@@ -4,11 +4,11 @@ export const registerUserProps = z.object({
   firstName: z.string().min(1).max(25).nonempty(),
   lastName: z.string().min(1).max(25).nonempty(),
   email: z.string().email().max(50).nonempty(),
-  password: z.string().min(6).max(20).nonempty(),
+  password: z.string().min(1).max(20).nonempty(),
   role: z.enum(['admin', 'user']).optional(),
 });
 
 export const loginUserProps = z.object({
   email: z.string().email().max(50).nonempty(),
-  password: z.string().min(6).max(20).nonempty(),
+  password: z.string().min(1).max(20).nonempty(),
 });
